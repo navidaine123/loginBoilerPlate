14099849a0a7fbe1e5eca669f2e60da4a5a3a35f
Navid Ainevand <navidaine123@gmail.com>
2020-09-12 14:00:19 +0430

    commit

--- server/middleware/usermiddleware.ts
+++ server/middleware/usermiddleware.ts
@@ -1,7 +1,8 @@
 import { Context } from "https://deno.land/x/oak@v6.1.0/context.ts";
-import { validateJwt, parseAndDecode } from "https://deno.land/x/djwt/validate.ts";
+import { validateJwt, parseAndDecode} from "https://deno.land/x/djwt/validate.ts";
 import { Status } from "https://deno.land/std@0.67.0/http/http_status.ts";
 import { users } from "../model/user.model.ts";
+import { User } from "../model/user.model.ts"
 
 const userMiddleware = async (ctx: Context, next: Function) => {
   const jwt = ctx.request.headers.get("Authorization")?.split('Bearer ')[1];
@@ -9,12 +10,13 @@ const userMiddleware = async (ctx: Context, next: Function) => {
   if (jwt) {
     const validate = await validateJwt({ jwt, key: "your-secret", algorithm: "HS256" });
     const data = parseAndDecode(validate.jwt + "");
-    console.log(data);
-
+    const a = data.payload as jwtPayload;
     if(data){
-
+      const user = users.find((u:User) => u.username  === a.iss);
+      ctx.state.currentuser = user;
     }
     else{
+      ctx.response.status = 500;
     }
   }
   
@@ -22,3 +24,8 @@ const userMiddleware = async (ctx: Context, next: Function) => {
 };
 
 export default userMiddleware;
+
+interface jwtPayload  {
+  iss: string;
+  exp: number;
+}
